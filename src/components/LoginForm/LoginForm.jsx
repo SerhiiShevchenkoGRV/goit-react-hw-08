9b8b39ac81f6803ce86bbfacb4e288d932a9dbaa
Serhii Shevchenko <sergey.groove.shevchenko@gmail.com>
2025-05-17 16:47:20 +0200

    update

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Field, Form, Formik } from "formik";
 import { login } from "../../redux/auth/operations";
 import toast from "react-hot-toast";
@@ -7,7 +7,10 @@ import s from "./LoginForm.module.css";
 
 export default function LoginForm() {
   const navigate = useNavigate();
+  const location = useLocation();
   const dispatch = useDispatch();
+  const from = location.state?.from?.pathname || "/contacts";
+
   const initialValues = {
     email: "",
     password: "",
@@ -17,7 +20,7 @@ export default function LoginForm() {
     dispatch(login(values))
       .unwrap()
       .then(() => {
-        navigate("/contacts", { replace: true });
+        navigate(from, { replace: true });
       })
       .catch(() => toast.error("Invalid data"));
     actions.resetForm();
--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,12 +1,18 @@
 import { useSelector } from "react-redux";
 import { selectIsLoggedIn, selectIsRefreshing } from "../redux/auth/selectors";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 export default function PrivateRoute({ children }) {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   const isRefreshing = useSelector(selectIsRefreshing);
+  const location = useLocation();
+
   if (isRefreshing) {
     return <h1>Loading...</h1>;
   }
-  return isLoggedIn ? children : <Navigate to="/login" />;
+  return isLoggedIn ? (
+    children
+  ) : (
+    <Navigate to="/login" replace state={{ from: location }} />
+  );
 }
