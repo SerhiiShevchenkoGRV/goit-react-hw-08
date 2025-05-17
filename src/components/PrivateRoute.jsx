0d033637937fa50620f11a29306b2ae68138a11a
Serhii Shevchenko <sergey.groove.shevchenko@gmail.com>
2025-05-17 16:26:58 +0200

    update

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -1,8 +1,12 @@
 import { useSelector } from "react-redux";
-import { selectIsLoggedIn } from "../redux/auth/selectors";
+import { selectIsLoggedIn, selectIsRefreshing } from "../redux/auth/selectors";
 import { Navigate } from "react-router-dom";
 
 export default function PrivateRoute({ children }) {
   const isLoggedIn = useSelector(selectIsLoggedIn);
+  const isRefreshing = useSelector(selectIsRefreshing);
+  if (isRefreshing) {
+    return <h1>Loading...</h1>;
+  }
   return isLoggedIn ? children : <Navigate to="/login" />;
 }
